c7b1e604ba02f7ea368267d6cfb303197cf514f8
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2024-01-18 21:37:23 -0500

    Change light mode accent to blue

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,26 @@
-import starlightPlugin from '@astrojs/starlight-tailwind';
+import starlightPlugin from "@astrojs/starlight-tailwind";
 
 // Generated color palettes
-const accent = { 200: '#98d7cb', 600: '#007d6f', 900: '#003c34', 950: '#002b25' };
-const gray = { 100: '#f3f6fe', 200: '#e7eefd', 300: '#b9c2d4', 400: '#7b8cac', 500: '#495876', 700: '#2a3854', 800: '#192641', 900: '#121824' };
+const accent = {
+  200: "#98d7cb",
+  600: "#0070b5",
+  900: "#003c34",
+  950: "#002b25",
+};
+const gray = {
+  100: "#f3f6fe",
+  200: "#e7eefd",
+  300: "#b9c2d4",
+  400: "#7b8cac",
+  500: "#495876",
+  700: "#2a3854",
+  800: "#192641",
+  900: "#121824",
+};
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       colors: { accent, gray },
